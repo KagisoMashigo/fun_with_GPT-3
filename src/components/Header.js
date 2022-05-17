3e8501252b0c1513cedc44f6707b0f0108364c06
Iso Mashigo <kagiso.k.mashigo@gmail.com>
2022-05-17 17:24:55 -0400

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import '../styles/Header.css';
+import React from "react";
+import "../styles/Header.css";
 
 const Header = () => {
-
   return (
     <div className="header">
       <h2 className="h2">Fun with A.I.</h2>
-      <div className="app-description">GPT-3 is a powerful AI model created by <a href="https://openai.com/api/">OpenAI</a>. It can process plain text prompts and produce outputs that are hard to distinguish from human writing. Check out some <a href="https://beta.openai.com/examples/">examples</a> of what it can do then come right back and give it a shot! 
+      <div className="app-description">
+        GPT-3 is a powerful AI model created by{" "}
+        <a href="https://openai.com/api/">OpenAI</a>. It can process plain text
+        prompts and produce outputs that are hard to distinguish from human
+        writing. Check out some{" "}
+        <a href="https://beta.openai.com/examples/">examples</a> of what it can
+        do then come right back and give it a shot!
       </div>
     </div>
   );
-}
-  
-export default Header
+};
+
+export default Header;
